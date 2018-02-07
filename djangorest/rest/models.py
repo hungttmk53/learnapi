5fb75d0aa0b1cef96605e00b456c45c758519e5c
hungdcwindows <hungttmk53@gmail.com>
2018-02-07 17:23:25 +0700

    OK

--- djangorest/rest/models.py
+++ djangorest/rest/models.py
@@ -3,3 +3,14 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=120)
+	content = models.TextField()
+	draft = models.BooleanField()
+	read_time = models.IntegerField(default=0)
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+	created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		ordering = ["-created", "-updated"]
